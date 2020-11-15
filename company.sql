bf6fccb8d58c01cc0ea2e8991cd46a1960dc0529
jaeung <lju9176@naver.com>
2020-11-15 21:11:19 +0900

    네번째

--- company.sql
+++ company.sql
@@ -11,3 +11,7 @@ create table dept(
 insert into dept values('001', '기획부', '홍길동');
 insert into dept values('002', '총무부', '이경성');
 insert into dept values('003', '인사부', '한이성');
+
+insert into dept values('004', '경리부', '김성경');
+
+select * from dept:
